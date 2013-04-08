585ff1650182a23555ff39b41f2e85ac1457be93
Luite Stegeman <stegeman@gmail.com>
2013-04-08 09:19:32 +0200

    expand foreign stuff, store js object references in single variable

--- GHCJS/Foreign.hs
+++ GHCJS/Foreign.hs
@@ -1,18 +1,14 @@
-{-# LANGUAGE ForeignFunctionInterface, CPP, MagicHash, FlexibleInstances, BangPatterns #-}
-
-{-
-  Utilities for interaction with JavaScript objects.
-  Objects are stored in an Addr# primitive, where the
-  second (offset) field is unused.
--}
+{-# LANGUAGE ForeignFunctionInterface, UnliftedFFITypes, CPP, MagicHash, FlexibleInstances, BangPatterns #-}
 
 module GHCJS.Foreign ( ToJSString(..)
                      , FromJSString(..)
-                     , Ptr'(..)
-                     , fromPtr
-                     , toPtr) where
+                     , fromJSBool
+                     , fromJSBool'
+                     , toJSBool
+                     ) where
 
 import GHCJS.Types
+import GHCJS.Types.Internal
 
 import qualified Data.Text as T
 import Foreign.Ptr
@@ -22,18 +18,26 @@ import GHC.Exts
 
 import qualified Data.Text.Array as A
 
-#if __GHCJS__==1
-foreign import javascript unsafe "$r = h$toStr($1,$1_2,$2); $r2 = 0;"
-  js_toString :: Ptr () -> Int -> Ptr ()
-
-foreign import javascript unsafe "$r = h$fromStr($1); $r2 = h$ret1;"
-  js_fromString :: Ptr () -> Ptr ()
+#ifdef __GHCJS__
+foreign import javascript unsafe "$r = h$toStr($1,$2,$3);" js_toString :: Ref# -> Int# -> Int# -> Ref#
+foreign import javascript unsafe "$r = h$fromStr($1); $r2 = h$ret1;" js_fromString :: Ref# -> Ptr ()
+foreign import javascript unsafe "($1 === true) ? 1 : 0" js_fromBool :: JSBool -> Int#
+foreign import javascript unsafe "$1 ? 1 : 0" js_isTruthy :: JSRef a -> Int#
+foreign import javascript unsafe "$r = true"  js_true :: Int# -> Ref#
+foreign import javascript unsafe "$r = false" js_false :: Int# -> Ref#
 #else
-js_toString :: Ptr () -> Int -> Ptr ()
-js_toString = error "only available in JavaScript"
-
-js_fromString :: Ptr () -> Ptr ()
-js_fromString = error "only available in JavaScript"
+js_toString :: Ref# -> Int# -> Int# -> Ref#
+js_toString = error "js_toString: only available in JavaScript"
+js_fromString :: Ref# -> Ptr ()
+js_fromString = error "js_fromString: only available in JavaScript"
+js_fromBool :: JSBool -> Int#
+js_fromBool = error "js_fromBool: only available in JavaScript"
+js_isTruthy :: JSRef a -> Int#
+js_isTruthy = error "js_isTruthy: only available in JavaScript"
+js_true :: Int# -> Ref#
+js_true = error "js_true: only available in JavaScript"
+js_false :: Int# -> Ref#
+js_false = error "js_false: only available in JavaScript"
 #endif
 
 class ToJSString a where
@@ -44,19 +48,40 @@ class FromJSString a where
 
 instance ToJSString [Char] where
   toJSString = toJSString . T.pack
+  {-# INLINE toJSString #-}
 
 instance FromJSString [Char] where
   fromJSString = T.unpack . fromJSString
+  {-# INLINE fromJSString #-}
 
 instance ToJSString T.Text where
   toJSString t =
-    let !(Text' (Array' b) (I# offset) length) = unsafeCoerce t
-    in  castPtr $ (js_toString (toPtr $ Ptr' b offset) length)
-
+    let !(Text'' (Array'' b) (I# offset) (I# length)) = unsafeCoerce t
+    in  mkRef (js_toString b offset length)
+  {-# INLINE toJSString #-}
+  
 instance FromJSString T.Text where
-  fromJSString p =
-    let !(Ptr' ba l) = fromPtr (js_fromString $ castPtr p)
+  fromJSString (JSRef (D# ref)) =
+    let !(Ptr' ba l) = ptrToPtr' (js_fromString ref)
     in  unsafeCoerce (Text' (Array' ba) 0 (I# l))
+  {-# INLINE fromJSString #-}
+fromJSBool :: JSBool -> Bool
+fromJSBool b = case js_fromBool b of
+                 1# -> True
+                 _  -> False
+{-# INLINE fromJSBool #-}
+
+toJSBool :: Bool -> JSBool
+toJSBool True = mkRef (js_true 0#)
+toJSBool _    = mkRef (js_false 0#)
+{-# INLINE toJSBool #-}
+
+-- check whether a reference is `truthy' in the JavaScript sense
+fromJSBool' :: JSRef a -> Bool
+fromJSBool' b = case js_isTruthy b of
+                  1# -> True
+                  _  -> False
+{-# INLINE fromJSBool' #-}
 
 -- something that we can unsafeCoerce Text from
 data Text' = Text'
@@ -68,11 +93,21 @@ data Array' = Array' {
       aBA :: ByteArray#
     }
 
+data Text'' = Text''
+    {-# UNPACK #-} !Array''          -- payload
+    {-# UNPACK #-} !Int              -- offset
+    {-# UNPACK #-} !Int              -- length
+
+data Array'' = Array'' {
+      aRef :: Ref#
+    }
+
 -- same rep as Ptr Addr#, use this to get just the first field out
-fromPtr :: Ptr a -> Ptr' b
-fromPtr = unsafeCoerce
-
-toPtr :: Ptr' a -> Ptr b
-toPtr = unsafeCoerce
-
 data Ptr' a = Ptr' ByteArray# Int#
+
+ptrToPtr' :: Ptr a -> Ptr' b
+ptrToPtr' = unsafeCoerce
+
+ptr'ToPtr :: Ptr' a -> Ptr b
+ptr'ToPtr = unsafeCoerce
+
--- GHCJS/Types.hs
+++ GHCJS/Types.hs
@@ -1,8 +1,33 @@
 {-# LANGUAGE EmptyDataDecls #-}
 
-module GHCJS.Types where
+module GHCJS.Types ( JSRef
+                   , isNull
+                   , isUndefined
+                   , eqRef
+                   , nullRef
+                   , castRef
+                   , JSString
+                   , JSObject
+--                   , JSObject'
+                   , JSBool
+                   , JSNumber
+                   )
+                 where
 
-import Foreign.Ptr
+import GHCJS.Types.Internal
 
-data JSChar
-type JSString = Ptr JSChar
+data JSBool_
+data JSNumber_
+data JSString_
+data JSObject_ a
+
+type JSBool     = JSRef JSBool_
+type JSNumber   = JSRef JSNumber_
+type JSString   = JSRef JSString_
+type JSObject a = JSRef (JSObject_ a)
+-- type JSObject'  = JSRef (JSObject (Any *))
+
+
+
+
+
