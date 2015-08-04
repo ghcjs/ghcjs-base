b73bdfd1d5d8ea81098bdc63d09455ebafc6a7cd
Oliver Charles <ollie@ocharles.org.uk>
2015-08-04 12:17:25 +0100

    Add time stamps to requestAnimationFrame
    requestAnimationFrame provides its callback with a DOMHighResTimeStamp,
    measuring a monotonic clock time for each frame. I've extended the
    bindings such that waitForAnimationFrame and inAnimationFrame provide
    this value to the caller.

--- jsbits/animationFrame.js
+++ jsbits/animationFrame.js
@@ -1,18 +1,18 @@
 function h$animationFrameCancel(h) {
     if(h.handle) window.cancelAnimationFrame(h.handle);
     if(h.callback) {
-	h$release(h.callback)
-	h.callback = null;
+        h$release(h.callback)
+        h.callback = null;
     }
 }
 
 function h$animationFrameRequest(h) {
-    h.handle = window.requestAnimationFrame(function() {
-	var cb = h.callback;
-	if(cb) {
-	    h$release(cb);
-	    h.callback = null;
-	    cb();
-	}
+    h.handle = window.requestAnimationFrame(function(ts) {
+        var cb = h.callback;
+        if(cb) {
+	        h$release(cb);
+	        h.callback = null;
+	        cb(ts);
+        }
     });
 }
