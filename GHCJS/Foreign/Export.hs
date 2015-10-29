05d8df85e7202320c5bcecb53596aa1a99266472
Luite Stegeman <stegeman@gmail.com>
2015-10-29 11:06:24 +0000

    make Export newtype and fix unsafeCoerce bug when dereferencing exports

--- GHCJS/Foreign/Export.hs
+++ GHCJS/Foreign/Export.hs
@@ -30,8 +30,10 @@ import Unsafe.Coerce
 import qualified GHC.Exts as Exts
 
 import GHCJS.Prim
+import GHCJS.Types
 
-type Export a = JSVal
+newtype Export a = Export JSVal
+instance IsJSVal (Export a)
 
 {- |
      Export any Haskell value to a JavaScript reference without evaluating it.
@@ -66,7 +68,7 @@ derefExport e = do
   r <- js_derefExport w1 w2 e
   if isNull r
     then return Nothing
-    else unsafeCoerce (js_toHeapObject r)
+    else unsafeCoerce <$> js_toHeapObject r
 
 {- |
      Release all memory associated with the export. Subsequent calls to
@@ -82,10 +84,10 @@ foreign import javascript unsafe
   js_export :: Word64 -> Word64 -> Any -> IO (Export a)
 foreign import javascript unsafe
   "h$derefExport"
-  js_derefExport :: Word64 -> Word64 -> JSVal -> IO JSVal
+  js_derefExport :: Word64 -> Word64 -> Export a -> IO JSVal
 foreign import javascript unsafe
-  "$r = $1;" js_toHeapObject :: JSVal -> Exts.Any
+  "$r = $1;" js_toHeapObject :: JSVal -> IO Exts.Any
 
 foreign import javascript unsafe
   "h$releaseExport"
-  js_releaseExport :: JSVal -> IO ()
+  js_releaseExport :: Export a -> IO ()
