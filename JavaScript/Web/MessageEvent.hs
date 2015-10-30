41f53449688e5c4b78f5748206b79345918544c8
Luite Stegeman <stegeman@gmail.com>
2015-10-30 22:53:36 +0000

    export getData for MessageEvent

--- JavaScript/Web/MessageEvent.hs
+++ JavaScript/Web/MessageEvent.hs
@@ -4,6 +4,7 @@
   #-}
 
 module JavaScript.Web.MessageEvent ( MessageEvent
+                                   , getData
                                    , MessageEventData(..)
                                    ) where
 
