85552b52ed6712d2f85797c19629ae345a65c315
achirkin <achirkin@users.noreply.github.com>
2015-11-03 16:34:52 +0100

    small fixes to arrays

--- jsbits/array.js
+++ jsbits/array.js
@@ -7,7 +7,7 @@
 function h$fromArray(a) {
     var r = HS_NIL;
     for(var i=a.length-1;i>=0;i--) r = MK_CONS(MK_JSVAL(a[i]), r);
-    return a;
+    return r;
 }
 
 /*
@@ -19,7 +19,7 @@ function h$fromArray(a) {
 function h$fromArrayNoWrap(a) {
     var r = HS_NIL;
     for(var i=a.length-1;i>=0;i--) r = MK_CONS(a[i], r);
-    return a;
+    return r;
 }
 
 /*
