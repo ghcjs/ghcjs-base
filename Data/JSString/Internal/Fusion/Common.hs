35b92c815d0b4736522fc0d11c2fab9af422cb7f
Luigy Leon <luigy@outlook.com>
2018-08-22 00:20:06 -0400

    Update toTitle to match latest Text

--- Data/JSString/Internal/Fusion/Common.hs
+++ Data/JSString/Internal/Fusion/Common.hs
@@ -95,7 +95,7 @@ import Prelude (Bool(..), Char, Eq(..), Int, Integral, Maybe(..),
 import qualified Data.List as L
 import qualified Prelude as P
 import Data.Bits (shiftL)
-import Data.Char (isLetter)
+import Data.Char (isLetter, isSpace)
 import Data.Int (Int64)
 import Data.JSString.Internal.Fusion.CaseMapping
   (foldMapping, lowerMapping, titleMapping, upperMapping)
@@ -449,11 +449,12 @@ toTitle (Stream next0 s0) = Stream next (CC (False :*: s0) '\0' '\0')
         Done           -> Done
         Skip s'        -> Skip (CC (letter :*: s') '\0' '\0')
         Yield c s'
-          | letter'    -> if letter
-                          then lowerMapping c (letter' :*: s')
+          | nonSpace   -> if letter
+                          then lowerMapping c (nonSpace :*: s')
                           else titleMapping c (letter' :*: s')
           | otherwise  -> Yield c (CC (letter' :*: s') '\0' '\0')
-          where letter' = isLetter c
+          where nonSpace = P.not (isSpace c)
+                letter' = isLetter c
     next (CC s a b)     = Yield a (CC s b '\0')
 {-# INLINE [0] toTitle #-}
 
