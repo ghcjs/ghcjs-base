1dc1aca50c5954cc219c6047bbf3c115191a1e63
Josh Meredith <joshmeredith2008@gmail.com>
2023-11-02 19:13:20 +1100

    Fixes

--- test/compat.js
+++ test/compat.js
@@ -5,3 +5,11 @@ function h$_hs_text_measure_off() {
 function h$splitmix_init() {
   return Math.floor(Math.random() * 0 * 100000000);
 }
+
+function MK_TUP2(x1, x2) {
+  h$c2(h$ghczmprimZCGHCziTupleziPrimziZ2T_con_e, x1, x2);
+}
+
+function MK_TUP3(x1, x2, x3) {
+  h$c3(h$ghczmprimZCGHCziTupleziPrimziZ3T_con_e, x1, x2, x3);
+}
